190db26a13c3a5b4376e1b62b51b46e7c9e70617
Stefan Dzhestanov <stefan.dzhestanov@waitrose.co.uk>
2020-03-08 22:55:02 +0000

    Formats rate

--- src/components/ExchangeRate/index.js
+++ src/components/ExchangeRate/index.js
@@ -4,12 +4,18 @@ import Icon from 'components/Icon';
 import * as currencies from 'constants/currencies';
 import styles from './exchangeRate.css';
 
-const ExchangeRate = ({ from, rate, to }) => (
-  <section className={styles.container}>
-    <Icon name="IncliningArrow" className={styles.icon} />
-    {`${currencies[from].symbol}1 = ${currencies[to].symbol}${rate}`}
-  </section>
-);
+const ExchangeRate = ({ from, rate, to }) => {
+  const formattedRate = (+rate).toFixed(4).toString();
+  const twoPartsRate = formattedRate.split(/(\d+[.]\d{2})(\d{2})/g);
+  console.log(formattedRate);
+  return (
+    <section className={styles.container}>
+      <Icon name="IncliningArrow" className={styles.icon} />
+      {`${currencies[from].symbol}1 = ${twoPartsRate[1]}`}
+      <span className={styles.rateSmaller}>{twoPartsRate[2]}</span>
+    </section>
+  );
+};
 
 ExchangeRate.propTypes = {
   from: string.isRequired,
